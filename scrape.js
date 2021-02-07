4a8c29efe35674c643e1a3c8e1ed00e2c948ec5e
Steph <steph@Stephs-MBP.attlocal.net>
2021-02-06 21:37:04 -0800

    create scraped html directory if it doesn't exist

--- scrape.js
+++ scrape.js
@@ -2,14 +2,20 @@ const fs = require("fs");
 
 const axios = require("axios");
 
+const scrapedDirectory = "scraped-html";
+
+if (!fs.existsSync(scrapedDirectory)) {
+  fs.mkdirSync(scrapedDirectory);
+}
+
 axios("https://www.dmv.ca.gov").then((res) =>
-  fs.writeFileSync("scraped-html/dmv.html", res.data)
+  fs.writeFileSync(`${scrapedDirectory}/dmv.html`, res.data)
 );
 
 axios("https://www.google.com").then((res) =>
-  fs.writeFileSync("scraped-html/google.html", res.data)
+  fs.writeFileSync(`${scrapedDirectory}/google.html`, res.data)
 );
 
 axios("https://www.nasa.gov").then((res) =>
-  fs.writeFileSync("scraped-html/nasa.html", res.data)
+  fs.writeFileSync(`${scrapedDirectory}/nasa.html`, res.data)
 );
