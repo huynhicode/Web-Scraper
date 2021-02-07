4399818499ccd4d621c9ac3f8202919c7aa4114e
Steph <steph@Stephs-MacBook-Pro.local>
2021-02-07 15:30:50 -0800

    clean up files and update readme

---
+++ server.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/scrape", (req, res) => {
+  const { url } = req.body;
+
+  axios(url)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch(console.error);
+});
+
+app.listen(5000, () => {
+  console.log("Server running on Port 5000");
+});
